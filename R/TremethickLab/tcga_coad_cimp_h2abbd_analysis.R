3371dbc0c8f9e6c74b6b80a363ebda58210b5b00
Sebastian Kurscheid <u1001407@mmlab4.anu.edu.au>
2015-03-23 16:13:35 +1100

    New comments in file header, GPL etc

--- R/TremethickLab/tcga_coad_cimp_h2abbd_analysis.R
+++ R/TremethickLab/tcga_coad_cimp_h2abbd_analysis.R
@@ -1,7 +1,26 @@
+#  tcga_coad_cimp_h2abbd_analysis.R
+#
+#  Copyright 2015 Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
 
 library(ade4)
 library(cluster)
 library(matlab)
+library(GenomicRanges)
 
 load("~/Data/Annotations/Platforms/gr.annot450k.rda")
 gr1 <- gr.annot450k[which(gr.annot450k$Probe_SNPs == "" & gr.annot450k$Probe_SNPs_10 == "")]
