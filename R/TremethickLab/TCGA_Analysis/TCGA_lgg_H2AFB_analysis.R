81a841ba734a712c93f4581253c7d63356677f6b
Sebastian Kurscheid <genomicsio@gmail.com>
2015-04-15 11:06:48 +1000

    made changes to source "heatmap.3" from the new GeneralPurpose/R repository

--- R/TremethickLab/TCGA_Analysis/TCGA_lgg_H2AFB_analysis.R
+++ R/TremethickLab/TCGA_Analysis/TCGA_lgg_H2AFB_analysis.R
@@ -16,7 +16,9 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+
+source("/Users/u1001407/Dropbox/Development/GeneralPurpose/R/heatmap.3.R")
+
 # load libraries
 
 require(vegan)
--- R/TremethickLab/TCGA_Analysis/tcga_prad_cimp_h2abbd_analysis.R
+++ R/TremethickLab/TCGA_Analysis/tcga_prad_cimp_h2abbd_analysis.R
@@ -17,6 +17,8 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
+source("/Users/u1001407/Dropbox/Development/GeneralPurpose/R/heatmap.3.R")
+
 library(ade4)
 library(cluster)
 library(matlab)
